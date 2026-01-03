6522a33bce9488aba7decf24ba4b37eb97e3b6ef
Berker Katipoglu <katipber@gmail.com>
2026-01-03 21:03:10 +0000

    🔥 initial commit 🔥

---
+++ ash_reactor_error/lib/ash_reactor_error.ex
@@ -0,0 +1,18 @@
+defmodule AshReactorError do
+  @moduledoc """
+  Documentation for `AshReactorError`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AshReactorError.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ ash_reactor_error/test/ash_reactor_error_test.exs
@@ -0,0 +1,8 @@
+defmodule AshReactorErrorTest do
+  use ExUnit.Case
+  doctest AshReactorError
+
+  test "greets the world" do
+    assert AshReactorError.hello() == :world
+  end
+end
